e7ffb91c5df3d1b828512724251cf299b97641cd
ANU. H A <125720369+anuanuha@users.noreply.github.com>
2023-09-24 21:34:33 +0530

    power of given number

---
+++ power of given number.java
@@ -0,0 +1,10 @@
+package inbuiltmathods;
+
+public class powerofgivennumber {
+	public static void main(String[] args) {
+		int a=2;
+		int pow=3;
+		System.out.println("power is "+(int) Math.pow(a, pow));
+	}
+
+}
