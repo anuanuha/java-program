c4f164f24ae389f1ff93ee23cbd6fd9ad7ade888
ANU. H A <125720369+anuanuha@users.noreply.github.com>
2023-09-24 21:30:31 +0530

    convert string to number

---
+++ convert string to number.java
@@ -0,0 +1,10 @@
+package objectclass;
+
+public class stringToNumber {
+	public static void main(String[] args) {
+		String s1="6752798";
+		int n =Integer.parseInt(s1);
+		System.out.println(n);
+	}
+
+}
